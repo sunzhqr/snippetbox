6a8c764be0cbb7f537181b6fad76343fecbf9cae
sunzhqr <sanzhar_myrzash@mail.ru>
2024-10-08 17:30:01 +0500

    chapter 2

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,11 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fmt.Println(fileServer)
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	fmt.Println("Starting server on http://localhost:4000/")
 	err := http.ListenAndServe(":4000", mux) // Web Server
 	log.Fatal(err)
